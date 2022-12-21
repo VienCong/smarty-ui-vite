4eb19248d27b0ebdc3e7e0decaee37710c6191d1
VienCong <wangjincong0110@163.com>
2022-12-21 21:35:32 +0800

    feat: add vitepress-theme-demoblock

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,16 @@
 import DefaultTheme from "vitepress/theme";
 import "vitepress-theme-demoblock/dist/theme/styles/index.css";
-
 import SmartyUI from "../../../src/entry";
+
+// 插件的组件，主要是demo组件
+import Demo from "vitepress-theme-demoblock/dist/client/components/Demo.vue";
+import DemoBlock from "vitepress-theme-demoblock/dist/client/components/DemoBlock.vue";
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.use(SmartyUI);
-    // app.component("Demo", Demo);
-    // app.component("DemoBlock", DemoBlock);
+  enhanceApp(ctx) {
+    ctx.app.use(SmartyUI);
+    ctx.app.component("Demo", Demo);
+    ctx.app.component("DemoBlock", DemoBlock);
   },
 };
